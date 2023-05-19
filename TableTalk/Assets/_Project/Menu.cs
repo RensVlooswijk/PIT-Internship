daa606f42f90cd8066459ef255b4f73d24ffd230
RensVlooswijk <rensvlooswijk@gmail.com>
2023-05-19 15:45:10 +0200

    Added Menu Scene + enviroment

---
+++ TableTalk/Assets/_Project/Menu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InworldStart()
+    {
+        SceneManager.LoadScene("InworldAI");
+    }
+
+    public void ChatGPTStart(){
+        SceneManager.LoadScene("ChatGPT");
+    }
+
+    public void QuitGame()
+    {
+        // Quit the application
+        Application.Quit();
+    }
+
+}
